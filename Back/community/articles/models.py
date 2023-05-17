95aee9f73a265cac3e837e2c0a528acb577f5c16
ah9mon <eomgyeol97@gmail.com>
2023-05-17 16:24:06 +0900

    feat: Community project
    added new Project and App:
    
    - articles App
      - added Model
        - Article
        - Comment
      - added view
        - Article CRUD
        - Comment CRUD

---
+++ Back/community/articles/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    movie_id = models.IntegerField()
+    movie_title = models.TextField()
+    music_title = models.TextField()
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model) :
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    content = models.CharField(max_length=400)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
