f2f064951b066afb86e5688c07cd10a80edabd3c
ah9mon <eomgyeol97@gmail.com>
2023-05-18 17:09:02 +0900

    feat: Added Like Article function
    - Added Like Article function
      - Added Like model and serializer
      - Added like_article function in community/accounts/views.py

--- Back/community/articles/models.py
+++ Back/community/articles/models.py
@@ -11,8 +11,12 @@ class Article(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 
-class Comment(models.Model) :
+class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     content = models.CharField(max_length=400)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class Like(models.Model):
+    user_id = models.TextField()
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
--- Back/community/articles/serializers.py
+++ Back/community/articles/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Article, Comment
+from .models import Article, Comment, Like
 
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,5 +9,11 @@ class ArticleSerializer(serializers.ModelSerializer):
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
+        fields = '__all__'
+        read_only_fields = ('article',)
+
+class LikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Like
         fields = '__all__'
         read_only_fields = ('article',)
