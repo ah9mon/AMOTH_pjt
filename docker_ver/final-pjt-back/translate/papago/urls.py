f1706fadd3b564aebd811617d10ba5d604850d1a
ah9mon <eomgyeol97@gmail.com>
2023-06-23 18:38:26 +0900

    fix : change ' to " for unity

--- docker_ver/final-pjt-back/translate/papago/urls.py
+++ docker_ver/final-pjt-back/translate/papago/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('papago/detect', views.detect_to_use_papago, name='papago_detect'),
-    path('papago/translate', views.translate_to_use_papago, name='papago_translate'),
+    path("papago/detect", views.detect_to_use_papago, name="papago_detect"),
+    path("papago/translate", views.translate_to_use_papago, name="papago_translate"),
 ]
