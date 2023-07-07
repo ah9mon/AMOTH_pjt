e71efde4be29670a32a0833b2c1b5506fc8ea0a2
ah9mon <eomgyeol97@gmail.com>
2023-07-07 13:30:02 +0900

    fix(articles): refactor article-related views and URLs
    renamed and refactored functions in views.py to enhance clarity and code
    structure.
    updated URLs in urls.py to align with the refactored views.
    improved code readability and added relevant context for better
    understanding.

--- docker_ver/final-pjt-back/community/articles/urls.py
+++ docker_ver/final-pjt-back/community/articles/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 app_name = "articles"
+
 urlpatterns = [
-    path("articles", views.article_list, name="article_list"),
-    path("articles/<int:article_pk>", views.article_detail, name="article_detail"),
-    path("articles/<int:article_pk>/comments", views.comment_list, name="comment_list"),
-    path("comments/<int:comment_pk>", views.comment_detail, name="article_detail"),
-    # path("articles/like", views.like_article, name="like_article")
-    path("articles/<int:article_pk>/likes", views.like_article, name="like_article"),
+    path("articles", views.ArticleListCreateView.as_view(), name="article_list"),
+    path("articles/<int:pk>", views.ArticleRetrieveUpdateDeleteView.as_view(), name="article_detail"),
+    path("articles/<int:article_pk>/comments", views.CommentListCreateView.as_view(), name="comment_list"),
+    path("comments/<int:pk>", views.CommentRetrieveUpdateDeleteView.as_view(), name="comment_detail"),
+    path("articles/<int:article_pk>/likes", views.LikeArticleView.as_view(), name="like_article"),
 ]
--- docker_ver/final-pjt-back/community/articles/views.py
+++ docker_ver/final-pjt-back/community/articles/views.py
@@ -1,156 +1,169 @@
 from django.shortcuts import get_object_or_404
-
-# DRF
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-'''
-DRF의 @api_view 데코레이터는 CSRF 보호를 위한 토큰을 자동으로 처리해주는 기능을 제공 
-이를 통해 DELETE 및 PATCH와 같은 메서드에 대한 CSRF 보호를 간단하게 구현
-'''
-
+from rest_framework.views import APIView
 from rest_framework import status
-
-#Model
 from .models import Article, Comment, Like
-
-#Serializer
 from .serializers import ArticleSerializer, CommentSerializer, LikeSerializer
 
-# Create your views here.
-@api_view(["GET", "POST"])
-def article_list(request):
-    # 게시글 데이터 READ 요청
-    if request.method == "GET":
-        '''
-        user_id를 
-        params에 넣어서 보내기
-        '''
-        
+
+class ArticleListCreateView(APIView):
+    """
+    API view for listing and creating articles.
+    """
+
+    def get(self, request):
+        """
+        Handles GET requests to retrieve a list of articles.
+        """
+
         user_id = request.GET.get("user_id")
 
-        # user_id가 요청에 섞여왔으면 -> 마이페이지의 내가 쓴 게시글, 좋아요한 게시글 전체보기
-        if user_id :
-            my_articles = Article.objects.filter(user_id = user_id) # 나의 게시글
-            liked_article = Like.objects.filter(user_id = user_id) # 내가 좋아요한 게시글
+        if user_id:
+            # Get articles created by the user
+            my_articles = Article.objects.filter(user_id=user_id)
+            liked_articles = Like.objects.filter(user_id=user_id)
 
             my_serializer = ArticleSerializer(my_articles, many=True)
-            liked_serializer = LikeSerializer(liked_article, many=True)
+            liked_serializer = LikeSerializer(liked_articles, many=True)
 
             serializer_data = {
-                "my_articles" : my_serializer.data,
-                "liked_articles" : liked_serializer.data,
+                "my_articles": my_serializer.data,
+                "liked_articles": liked_serializer.data,
             }
-        
-        # user_id가 없이 왔으면 -> 게시글 전체보기
         else:
+            # Get all articles
             articles = Article.objects.all()
-
             serializer = ArticleSerializer(articles, many=True)
             serializer_data = serializer.data
 
         return Response(serializer_data, status=status.HTTP_200_OK)
-    
-    # 게시글 데이터 CREATE 요청
-    elif request.method == "POST": 
-        # movie_id랑 title만 받아서 저장하자 
-        serializer = ArticleSerializer(data = request.data) # 역직렬화
-        if serializer.is_valid(raise_exception=True): # 유효성 검사 / 실패하면 400code 
+
+    def post(self, request):
+        """
+        Handles POST requests to create a new article.
+        """
+
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED) # 직렬화해서 상태코드랑 같이 보내기
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(["GET","DELETE","PUT"])
-def article_detail(request, article_pk):
-    '''
-    DELETE, PUT에선 user_id 같이 보내줘야함
-    DELETE -> params에 넣어서 보내기 -> request.GET 으로 읽기
-    PUT -> body에 넣어서 보내기 -> request.POST or request.data 으로 읽기
-    '''
-    
-    article = get_object_or_404(Article, pk=article_pk)
-    user_id = request.GET.get("user_id")
-    # print(article.user_id)
-    '''
-    print(request.GET) # params에 있는거 request를 사전형 객체로 만들어줌 
-    print(request.POST) # body에 있는거 request를 사전형 객체로 만들어줌 
-    print(request.data) # body에 있는거 request를 사전형 객체로 만들어줌
-    '''
-    
-    # print(user_id)
 
-    # 게시글 상세 조회
-    if request.method == "GET":
+class ArticleRetrieveUpdateDeleteView(APIView):
+    """
+    API view for retrieving, updating, and deleting an article.
+    """
+
+    def get(self, request, pk):
+        """
+        Handles GET requests to retrieve an article by its primary key.
+        """
+
+        article = get_object_or_404(Article, pk=pk)
+        user_id = request.GET.get("user_id")
+
         liked = False
         likes = article.like_set.all()
-        # 현재 유저의 좋아요 여부 찾기
         for liked_article in likes:
             if liked_article.user_id == user_id:
                 liked = True
                 break
-        
+
         liked_count = len(likes)
-        
+
         serializer = ArticleSerializer(article)
 
         data = {
-                "article" : serializer.data,
-                "liked_count" : liked_count,
-                "liked" : liked,
-            }
+            "article": serializer.data,
+            "liked_count": liked_count,
+            "liked": liked,
+        }
         return Response(data, status=status.HTTP_200_OK)
-    
-    # 게시글 삭제
-    elif request.method == "DELETE":
-        
-        # 게시글 작성자 == 요청 user 면
-        if article.user_id == user_id: 
+
+    def delete(self, request, pk):
+        """
+        Handles DELETE requests to delete an article by its primary key.
+        """
+
+        article = get_object_or_404(Article, pk=pk)
+        user_id = request.GET.get("user_id")
+
+        if article.user_id == user_id:
             article.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
         else:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
-    
-    # 게시글 수정
-    elif request.method == "PUT":
-        # 게시글 작성자 == 요청 user 면 
+
+    def put(self, request, pk):
+        """
+        Handles PUT requests to update an article by its primary key.
+        """
+
+        article = get_object_or_404(Article, pk=pk)
+        user_id = request.GET.get("user_id")
+
         if article.user_id == user_id:
-            serializer = ArticleSerializer(article, data= request.data)
+            serializer = ArticleSerializer(article, data=request.data)
             if serializer.is_valid(raise_exception=True):
                 serializer.save()
                 return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
-        
-@api_view(["GET", "POST"])
-def comment_list(request, article_pk):
-    # 해당 게시글의 댓글 불러오기 
-    if request.method == "GET": 
-        comments = Comment.objects.filter(article = article_pk)
+
+
+class CommentListCreateView(APIView):
+    """
+    API view for listing and creating comments for an article.
+    """
+
+    def get(self, request, article_pk):
+        """
+        Handles GET requests to retrieve comments for an article.
+        """
+
+        comments = Comment.objects.filter(article=article_pk)
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    # 댓글 생성
-    elif request.method == "POST":
-        article = Article.objects.get(id=article_pk)
-        serializer = CommentSerializer(data = request.data)
+    def post(self, request, article_pk):
+        """
+        Handles POST requests to create a new comment for an article.
+        """
+
+        article = get_object_or_404(Article, id=article_pk)
+        serializer = CommentSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save(article=article)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(["DELETE", "PATCH"]) 
-def comment_detail(request, comment_pk):
-    comment = get_object_or_404(Comment, pk = comment_pk)
 
-    # 댓글 삭제 
-    if request.method == "DELETE":
+class CommentRetrieveUpdateDeleteView(APIView):
+    """
+    API view for retrieving, updating, and deleting a comment.
+    """
+
+    def delete(self, request, pk):
+        """
+        Handles DELETE requests to delete a comment by its primary key.
+        """
+
+        comment = get_object_or_404(Comment, pk=pk)
         user_id = request.GET.get("user_id")
-        if comment.user_id == user_id:           
+
+        if comment.user_id == user_id:
             comment.delete()
             return Response(status=status.HTTP_200_OK)
         else:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
-    
-    # 댓글 수정
-    elif request.method == "PATCH":
-        user_id = request.POST.get("user_id")
+
+    def patch(self, request, pk):
+        """
+        Handles PATCH requests to update a comment by its primary key.
+        """
+
+        comment = get_object_or_404(Comment, pk=pk)
+        user_id = request.data.get("user_id")
+
         if comment.user_id == user_id:
             serializer = CommentSerializer(comment, data=request.data, partial=True)
             if serializer.is_valid():
@@ -158,31 +171,31 @@ def comment_detail(request, comment_pk):
                 return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
-    
-@api_view(["POST"])
-def like_article(request,article_pk):
-    '''
-    좋아요/좋아요 취소 
-    '''
-    if request.method =="POST":
+
+
+class LikeArticleView(APIView):
+    """
+    API view for liking/unliking an article.
+    """
+
+    def post(self, request, article_pk):
+        """
+        Handles POST requests to like/unlike an article.
+        """
+
         user_id = request.data.get("user_id")
-        article_id = article_pk
-        likes = Like.objects.filter(user_id = user_id) # 요청 보낸 유저가 좋아요한 게시글 리스트 다 받기 
-        
-        # 해당 게시글 좋아요 여부 체크
-        for like in likes:
+        article = get_object_or_404(Article, id=article_pk)
 
-            # 좋아요 했으면 좋아요 취소 
-            if like.article.id == int(article_id):
-                like.delete()
-                return Response(status=status.HTTP_200_OK)
-
-        # 좋아요 안했으면 좋아요
-        article = Article.objects.get(id = int(article_id))
-        data = {
-            "user_id" : user_id,
-        }
-        serializer = LikeSerializer(data = data)
-        if serializer.is_valid():
-            serializer.save(article=article)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        likes = Like.objects.filter(user_id=user_id, article=article)
+        if likes.exists():
+            likes.delete()
+            return Response(status=status.HTTP_200_OK)
+        else:
+            data = {
+                "user_id": user_id,
+                "article": article_pk,
+            }
+            serializer = LikeSerializer(data=data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
