554a7c9572434eb9303fa38c8a28a467c1dac0fd
ah9mon <eomgyeol97@gmail.com>
2023-07-07 10:22:36 +0900

    fix(auth): refactor authentication views and URLs
    - renamed and refactored functions in views.py for improved clarity and
      code organization.
      - Updated URLs in urls.py to match the refactored views.
      - Added appropriate context and improved code readability.

--- docker_ver/final-pjt-back/auth/accounts/views.py
+++ docker_ver/final-pjt-back/auth/accounts/views.py
@@ -16,7 +16,6 @@ from .serializers import UserSerializer
 # requests
 import requests
 
-# KAKAO LOGIN API
 KAKAO_KEY = settings.KAKAO_KEY
 KAKAO_USER_URL = "https://kapi.kakao.com"
 KAKAO_AUTH_URL = "https://kauth.kakao.com"
@@ -29,200 +28,173 @@ NAVER_BASE_URL = "https://nid.naver.com/oauth2.0/"
 
 def get_userdata(access_token):
     '''
-    access_token으로 userdata 가져오는 함수
+    Get user data using the access_token.
 
-    param
-    access_token : 사용자 정보 요청할 때 사용하는 access_token
+    Parameters:
+    - access_token: Access token used to request user information.
+
+    Returns:
+    - User data obtained from the API.
     '''
-
-    Authorization = f"Bearer {access_token}"
-    header = {
-        'Authorization' : Authorization
-    }
-    user_url = f"{KAKAO_USER_URL}/v2/user/me"
-    # user 정보 요청 
-    user = requests.get(user_url, headers = header)
-
+    headers = {'Authorization': f'Bearer {access_token}'}
+    user_url = f'{KAKAO_USER_URL}/v2/user/me'
+    user = requests.get(user_url, headers=headers)
     return user
 
 def get_access_token(code):
     '''
-    인가 code로 access_token 가져오는 함수
+    Get access_token using the authorization code.
 
-    param
-    code : access_token 요청할 때 사용하는 인가코드
+    Parameters:
+    - code: Authorization code used to request the access_token.
+
+    Returns:
+    - Access token obtained from the API.
     '''
     data = {
-        "client_id" : KAKAO_KEY,
-        "grant_type" : "authorization_code",
-        "redirection_uri" : "http://127.0.0.1:8000/api/kakao",
-        "code" : code
+        'client_id': KAKAO_KEY,
+        'grant_type': 'authorization_code',
+        'redirection_uri': 'http://127.0.0.1:8000/api/kakao',
+        'code': code
     }
-    token_url = f"{KAKAO_AUTH_URL}/oauth/token"
-    
-    # post로 요청 
+    token_url = f'{KAKAO_AUTH_URL}/oauth/token'
     token_data = requests.post(token_url, data=data).json()
-
-    access_token = token_data.get("access_token")
-    # token_type = token_res.json().get('token_type')
-    # refresh_token = token_res.json().get('refresh_token')
-
+    access_token = token_data.get('access_token')
     return access_token
 
-###########################
-### 카카오 로그인 및 회원가입 ###
-###########################
+def process_kakao_login(request):
+    '''
+    Process Kakao login.
 
-def KakaoLogin(request):
-    # 1. 인가 코드 받기 요청 
-    redirect_uri = "http://127.0.0.1:8000/api/kakao/callback"
-    url = f"{KAKAO_AUTH_URL}/oauth/authorize?client_id={KAKAO_KEY}&redirect_uri={redirect_uri}&response_type=code"
+    Redirects to Kakao authorization URL to initiate the login process.
+    '''
+    redirect_uri = 'http://127.0.0.1:8000/api/kakao/callback'
+    url = f'{KAKAO_AUTH_URL}/oauth/authorize?client_id={KAKAO_KEY}&redirect_uri={redirect_uri}&response_type=code'
     return redirect(url)
 
-def KakaoCallback(request):
-    # 인가 코드 받아서 auth/kakao 에서 auth/kakao/callback으로 리다이렉트돼서 온거임
-     
-    # 2. 인가 코드로 access token 요청 
-    code = request.GET.get("code")
+def process_kakao_callback(request):
+    '''
+    Process the callback after Kakao login authorization.
+
+    Retrieves the access_token using the authorization code and fetches user data.
+    If the user does not exist, saves the user information in the database.
+    Redirects to the login page with the access_token and source as query parameters.
+    '''
+    code = request.GET.get('code')
     access_token = get_access_token(code)
-
-
-    # 3. access_token을 사용해서 사용자 정보 가져오기 
     user = get_userdata(access_token).json()
-
-    # 사용자 정보 Model 형식으로
-    nickname = user.get("properties").get("nickname")
-    profile_image = user.get("properties").get("profile_image")
-    email = user.get("kakao_account").get("email")
+    nickname = user.get('properties').get('nickname')
+    profile_image = user.get('properties').get('profile_image')
+    email = user.get('kakao_account').get('email')
     userdata = {
-        "nickname" : nickname,
-        "profile_image" : profile_image,
-        "email" : email
+        'nickname': nickname,
+        'profile_image': profile_image,
+        'email': email
     }
-
-    # 신규 사용자 회원가입 (DB에 저장)
-    serializer = UserSerializer(data = userdata) 
+    serializer = UserSerializer(data=userdata)
     users = User.objects.all()
     if serializer.is_valid(raise_exception=True):
-        existing_user = users.filter(email = email)
-        if not existing_user: # DB에 없는 사용자면 
-            print("회원가입!")
+        existing_user = users.filter(email=email)
+        if not existing_user:
             serializer.save()
-    
-    context = {
-        "access_token" : access_token 
-    }
-    
-    return HttpResponseRedirect(f"http://localhost:8080/login?access_token={access_token}&source=kakao")
+    redirect_url = f'http://localhost:8080/login?access_token={access_token}&source=kakao'
+    return HttpResponseRedirect(redirect_url)
 
-###########################
-##### 사용자 로그인 처리 #####
-###########################
+def process_kakao_auth(request):
+    '''
+    Process Kakao authentication.
 
-def KakaoAuth(request):
-    access_token = request.META.get("HTTP_AUTHORIZATION")
-
+    Retrieves the access_token from the request headers.
+    If the token is valid, fetches user data and returns it as a JSON response.
+    Otherwise, redirects to the login page.
+    '''
+    access_token = request.META.get('HTTP_AUTHORIZATION')
     user = get_userdata(access_token)
-
-    # 토큰이 유효해서 사용자 정보를 가져오기 성공했으면
     if user:
-        email = user.json().get("kakao_account").get("email")
-        userdata = get_object_or_404(User, email = email)
+        email = user.json().get('kakao_account').get('email')
+        userdata = get_object_or_404(User, email=email)
         serializer = UserSerializer(userdata)
         user_id = serializer.data.get('id')
-        profile_picture = serializer.data.get("profile_image")
+        profile_picture = serializer.data.get('profile_image')
         context = {
-            "user_id" : user_id,
-            "profile_picture" : profile_picture
+            'user_id': user_id,
+            'profile_picture': profile_picture
         }
         return JsonResponse(context, status=status.HTTP_200_OK)
-    
-    # 토큰이 유효하지 않으면 로그인 페이지로 redirect
     else:
-        login_url = "http://127.0.0.1:8080/login"
+        login_url = 'http://127.0.0.1:8080/login'
         return redirect(login_url)
 
-###########################
-### 네이버 로그인 및 회원가입 ###
-###########################
+def process_naver_login(request):
+    '''
+    Process Naver login.
 
-def NaverLogin(request):
-    # 1. 인가 코드 받기 요청 
-    redirect_uri = "http://127.0.0.1:8000/api/naver/callback"
-    url = f"{NAVER_BASE_URL}authorize?response_type=code&client_id={NAVER_CLIENT_ID}&redirect_uri={redirect_uri}&state=test"
+    Redirects to Naver authorization URL to initiate the login process.
+    '''
+    redirect_uri = 'http://127.0.0.1:8000/api/naver/callback'
+    url = f'{NAVER_BASE_URL}authorize?response_type=code&client_id={NAVER_CLIENT_ID}&redirect_uri={redirect_uri}&state=test'
     return redirect(url)
 
-def NaverCallback(request):
-    # 코드 받아왔음 
-    code = request.GET.get("code")
-    state = request.GET.get("state")
+def process_naver_callback(request):
+    '''
+    Process the callback after Naver login authorization.
 
-    # 코드로 토큰 요청 
-    url = f"{NAVER_BASE_URL}token?grant_type=authorization_code&client_id={NAVER_CLIENT_ID}&client_secret={NAVER_CLIENT_SECRET}&redirect_uri=http://127.0.0.1:8000/api/naver/callback&code={code}&state={state}"
+    Retrieves the access_token using the authorization code and fetches user data.
+    If the user does not exist, saves the user information in the database.
+    Redirects to the login page with the access_token and source as query parameters.
+    '''
+    code = request.GET.get('code')
+    state = request.GET.get('state')
+    url = f'{NAVER_BASE_URL}token?grant_type=authorization_code&client_id={NAVER_CLIENT_ID}&client_secret={NAVER_CLIENT_SECRET}&redirect_uri=http://127.0.0.1:8000/api/naver/callback&code={code}&state={state}'
     headers = {
-        "X-Naver-Client-Id":NAVER_CLIENT_ID, 
-        "X-Naver-Client-Secret": NAVER_CLIENT_SECRET
+        'X-Naver-Client-Id': NAVER_CLIENT_ID,
+        'X-Naver-Client-Secret': NAVER_CLIENT_SECRET
     }
     res = requests.get(url, headers=headers).json()
-    access_token = res.get("access_token")
-    # refresh_token = res.get("refresh_token")
-    # token_type = res.get("token_type")
-
-    # 회원 정보 요청 
-    url = "https://openapi.naver.com/v1/nid/me"
-    headers = {
-        "Authorization" : f"Bearer {access_token}"
-    }
+    access_token = res.get('access_token')
+    url = 'https://openapi.naver.com/v1/nid/me'
+    headers = {'Authorization': f'Bearer {access_token}'}
     user = requests.get(url, headers=headers).json()
-
-    # 사용자 정보 Model 형식으로
-    nickname = user.get("response").get("name")
-    profile_image = user.get("response").get("profile_image")
-    email = user.get("response").get("email")
+    nickname = user.get('response').get('name')
+    profile_image = user.get('response').get('profile_image')
+    email = user.get('response').get('email')
     userdata = {
-        "nickname" : nickname,
-        "profile_image" : profile_image,
-        "email" : email
+        'nickname': nickname,
+        'profile_image': profile_image,
+        'email': email
     }
-
-    # 신규 사용자 회원가입 (DB에 저장)
-    serializer = UserSerializer(data = userdata) 
+    serializer = UserSerializer(data=userdata)
     users = User.objects.all()
     if serializer.is_valid(raise_exception=True):
-        existing_user = users.filter(email = email)
-        if not existing_user: # DB에 없는 사용자면 
+        existing_user = users.filter(email=email)
+        if not existing_user:
             serializer.save()
+    redirect_url = f'http://localhost:8080/login?access_token={access_token}&source=naver'
+    return HttpResponseRedirect(redirect_url)
 
-    return HttpResponseRedirect(f"http://localhost:8080/login?access_token={access_token}&source=naver")
+def process_naver_auth(request):
+    '''
+    Process Naver authentication.
 
-##########################
-##### 사용자 로그인 처리 #####
-##########################
-
-def NaverAuth(request):
-    access_token = request.META.get("HTTP_AUTHORIZATION")
-
-    # 회원 정보 요청 
-    url = "https://openapi.naver.com/v1/nid/me"
-    headers = {
-        "Authorization" : f"Bearer {access_token}"
-    }
-    user = requests.get(url, headers=headers)
-
-    # 토큰이 유효해서 사용자 정보를 가져오기 성공했으면
+    Retrieves the access_token from the request headers.
+    If the token is valid, fetches user data and returns it as a JSON response.
+    Otherwise, redirects to the login page.
+    '''
+    access_token = request.META.get('HTTP_AUTHORIZATION')
+    url = 'https://openapi.naver.com/v1/nid/me'
+    headers = {'Authorization': f'Bearer {access_token}'}
+    user = requests.get(url, headers=headers).json()
     if user:
-        email = user.json().get("response").get("email")
-        userdata = get_object_or_404(User, email = email)
+        email = user.get('response').get('email')
+        userdata = get_object_or_404(User, email=email)
         serializer = UserSerializer(userdata)
-        user_id = serializer.data.get("id")
-        profile_picture = serializer.data.get("profile_image")
+        user_id = serializer.data.get('id')
+        profile_picture = serializer.data.get('profile_image')
         context = {
-            "user_id" : user_id,
-            "profile_picture" : profile_picture
+            'user_id': user_id,
+            'profile_picture': profile_picture
         }
         return JsonResponse(context, status=status.HTTP_200_OK)
-    
-    # 토큰이 유효하지 않으면 로그인 페이지로 redirect
     else:
-        login_url = "http://localhost:8080/login"
+        login_url = 'http://localhost:8080/login'
         return redirect(login_url)
